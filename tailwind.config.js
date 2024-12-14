001ae90b4f41966bd7551238b911b762543dd3b7
Raven Barrogo <barrogoraven@gmail.com>
2024-12-14 17:10:47 +0800

    CHANGE: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import animate from 'tailwindcss-animate'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
@@ -90,5 +92,5 @@ export default {
       }
     }
   },
-  plugins: [require('tailwindcss-animate')]
+  plugins: [animate]
 }
